7a84264b6b1f1b900d650b52cbe4db0820e5e5f8
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-22 21:32:53 +0100

    Adding config of app with json, and creating post request manager

--- api.js
+++ api.js
@@ -1,21 +1,24 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = process.env.PATHTOFOLDER;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     let fullPath = path;
     if (req.query.path) fullPath = path + req.query.path + '/';
-    console.log(fullPath)
     const content = fs.readdirSync(fullPath);
-    const folders = []
-    const files = []
+    const folders = [];
+    const files = [];
 
     content.forEach(recurso => {
         const comprobacionDirectorio = fs.lstatSync(fullPath + recurso).isDirectory();
         if (comprobacionDirectorio) folders.push(recurso);
         else files.push(recurso);
-    })
+    });
+
     res.json({
         path: req.query.path,
         folders: folders,
@@ -23,6 +26,12 @@ app.get('/', (req, res) => {
     });
 });
 
+app.post('/', (req, res) => {
+    console.log(req.body);
+    console.log(req.files);
+    console.log(req.query.path);
+})
+
 app.listen(process.env.PORT_APP || 3001, () => {
-    console.log('APP deployed in PORT :3001');
+    console.log('API Desplegada:', `http://localhost:3001`);
 });
