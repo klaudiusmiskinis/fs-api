69dd5ec752b0713cfab1c35abb7e9700679aeaa8
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-22 21:36:07 +0100

    Adding module bodyParser and setting config of app with this bodyParser

--- api.js
+++ api.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const bodyParser = require('body-parser');
 const path = process.env.PATHTOFOLDER;
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     let fullPath = path;
