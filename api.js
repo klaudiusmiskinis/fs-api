5bb06847112ea7604f895a6e97be621b52fd4f50
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-23 15:34:00 +0100

    Importing fileUpload, post and delete http methods working correctly

--- api.js
+++ api.js
@@ -1,21 +1,25 @@
 /* Imports */
 require('dotenv').config();
 const express = require('express');
+const fileupload = require("express-fileupload");
 const cors = require('cors');
-const app = express();
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const app = express();
 const path = process.env.PATHTOFOLDER; 
 
 /* Configuration */
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileupload());
 app.use(cors())
 
 /* HTTP Methods */
 app.get('/', (req, res) => {
     let fullPath = path;
     if (req.query.path) fullPath = path + req.query.path + '/';
+    fullPath = decodeURI(fullPath);
+    fullPath = fullPath.split('%20').join(' ')
     const content = fs.readdirSync(fullPath);
     const folders = [];
     const files = [];
@@ -25,7 +29,6 @@ app.get('/', (req, res) => {
         if (comprobacionDirectorio) folders.push(recurso);
         else files.push(recurso);
     });
-
     res.json({
         path: req.query.path,
         folders: folders,
@@ -33,8 +36,43 @@ app.get('/', (req, res) => {
     });
 });
 
-app.post('/', (req, res) => {
-    console.log(req.body);
+app.post('/', async (req, res) => {
+    let fullPath = path;
+    if (req.query.path) fullPath = path + req.query.path + '/';
+    fullPath = decodeURI(fullPath);
+    fullPath = fullPath.split('%20').join(' ')
+    const content = fs.readdirSync(fullPath);
+    const folders = [];
+    const files = [];
+    try {
+        await req.files.file.mv(fullPath + req.files.file.name)
+    } catch (error) {
+        return error;
+    }
+    res.json({
+        success: true,
+        message: "Some success message",
+        data: "some data if there's any"
+    });
+    res.end();
+})
+
+app.delete('/', (req, res) => {
+    let filePath = path;
+    if (req.query.path) filePath = filePath + req.query.path + '/';
+    if (req.query.file) filePath = filePath + req.query.file;
+    console.log('filePath:', filePath)
+    try {
+        fs.unlinkSync(filePath)
+    } catch(error) {
+        res.json({ok: false})
+    };
+    res.json({
+        success: true,
+        message: "Some success message",
+        data: "some data if there's any"
+    });
+    res.end();
 })
 
 app.listen(process.env.PORT, () => {
