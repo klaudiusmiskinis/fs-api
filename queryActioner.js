04bf3afc0c8413fa1567050c59588d8fa18decee
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-09 14:38:51 +0100

    Adding methods and test

--- queryActioner.js
+++ queryActioner.js
@@ -1,24 +1,73 @@
 class QueryActioner {
     query;
+    files;
+    action;
 
-    constructor(query) {
+    constructor(query, files, action) {
         this.query = query;
+        this.files = files;
+        this.action = action;
     }
 
     getQuery(){
         return this.query;
     }
 
+    getFiles() {
+        return this.files;
+    }
+
+    getAction(){
+        return this.action;
+    }
+
     setQuery(query) {
         this.query = query;
     }
+
+    setFiles(files) {
+        this.files = files;
+    }
+
+    setAction(action) {
+        this.action = action;
+    }
+
+    async actionAuto(action, body, files = null) {
+        switch (action) {
+            case 'create':
+                if (files) {
+                    await req.files.file.mv(process.env.PATHTOFOLDER + req.files.file.name)
+                } else if (body) {
+
+                }
+            case 'delete':
+                if (body) {
+                    
+                }
+            case 'edit':
+                if (body) {
+
+                }
+            default:
+                break;
+        }
+
+        if (body) {
+            console.log(body)
+        }
+
+        if (files) {
+            console.log(files)
+        }
+    }
 }
 
-let url = new QueryActioner(
-    [
-        {data: 'file', datainfo: 'datainfo'},
-        {data: 'folder', pacoinfo: 'pacoinfo'}
-    ]
-)
+/* CREATE */
+console.log(new QueryActioner().actionAuto('create', {data: 'file', datainfo: 'datainfo'}));
 
-console.log(url.getQuery())
+/* REMOVE */
+console.log(new QueryActioner({data: 'file', datainfo: 'datainfo'}).getQuery(), 'delete');
+
+/* UPDATE */
+console.log(new QueryActioner({file: {name: 'installer.exe'}}).getQuery(), 'create');
