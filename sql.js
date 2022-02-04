f733025f56ae84ba4fb66c246b28c7c480745ae5
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-02-04 12:54:10 +0100

    Creating sql.js to make databases queries, with 2 new functions. One for testing, another for select all from table archivos

--- sql.js
+++ sql.js
@@ -1,12 +1,31 @@
-config
-const { config } = require('dotenv');
-const mysql = require('mysql');
+require('dotenv').config();
+const util = require('util');
+const mysql = require('mysql2');
+const LoginMysql = {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PWD,
+    database: process.env.MYSQL_DB
+}
 
-const pool = mysql.createPool({
-    connectionLimit: 10,    
-    password:
-    user:
-    database:
-    host:
-    port:
-}); 
+module.exports.test = test = () => (async() => {
+    const conn = mysql.createConnection(LoginMysql);
+    try {
+        const query = util.promisify(conn.query).bind(conn);
+        const rows = await query('select count(*) as count from archivos');
+        return rows;
+    } finally {
+      conn.end();
+    }
+})()
+
+module.exports.selectAllFiles = selectAllFiles = () => (async() => {
+    const conn = mysql.createConnection(LoginMysql);
+    try {
+        const query = util.promisify(conn.query).bind(conn);
+        const rows = await query('select * from archivos');
+        return rows;
+    } finally {
+        conn.end();
+    }
+})
