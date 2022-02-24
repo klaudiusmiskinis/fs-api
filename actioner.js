bf84aff9b084937942326c93ab89c441bbeee73b
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-02-24 10:26:16 +0100

    Changing actioner and removing functionality of api get / to actioner

--- actioner.js
+++ actioner.js
@@ -1,23 +1,69 @@
 const fs = require('fs')
+module.exports.getFoldersAndFiles = getFoldersAndFiles;
+module.exports.pathChanger = pathChanger;
+module.exports.reading = reading;
+module.exports.error = error;
 
-module.exports = {
-    reading: function(path) {
-        const folders = []; 
-        const files = [];
-        const content = fs.readdirSync(path);
-        content.forEach(recurso => {
-            if (fs.lstatSync(path + recurso).isDirectory()) folders.push(recurso);
-            else files.push(recurso);
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ */
+function getFoldersAndFiles(req, res) {
+    let fullPath = process.env.PATHTOFOLDER;
+    let catchError = false;
+    let result;
+    if (req.query.path) fullPath = pathChanger(fullPath, req.query.path);
+    try {
+        result = reading(fullPath);
+    } catch(error) {
+        catchError = error(error);
+    };
+    if (catchError) {
+        res.status(200).json(failed);
+    } else if (!catchError) {
+        res.status(200).json({
+            success: true,
+            path: req.query.path,
+            folders: result[0],
+            files: result[1]
         });
-        return [folders, files];
-    },
+        res.end();
+    };
+}
 
-    pathChanger: function(path, query) {
-        return path + query + '/';
-    },
+/**
+ * 
+ * @param {*} path 
+ * @returns 
+ */
+function reading(path) {
+    const folders = []; 
+    const files = [];
+    const content = fs.readdirSync(path);
+    content.forEach(recurso => {
+        if (fs.lstatSync(path + recurso).isDirectory()) folders.push(recurso);
+        else files.push(recurso);
+    });
+    return [folders, files];
+}
 
-    error: function(error) {
-        console.log(error);
-        return true;
-    }
+/**
+ * 
+ * @param {*} path 
+ * @param {*} query 
+ * @returns 
+ */
+function pathChanger(path, query) {
+    return path + query + '/';
+}
+
+/**
+ * 
+ * @param {*} error 
+ * @returns 
+ */
+function error(error) {
+    console.log(error);
+    return true;
 }
