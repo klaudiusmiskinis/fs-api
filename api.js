8f631c52c17c9402215e520ac10df7c4435ce0f6
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-03-01 12:45:24 +0100

    Removing import not used and removing functions and replacing them with the actioner.

--- api.js
+++ api.js
@@ -1,5 +1,7 @@
 /* Imports */
 require('dotenv').config();
+const { extended, method, failed } = require('./config');
+const { getFoldersAndFiles, makeRecursive, status, download, check } = require('./actioner');
 const fileupload = require("express-fileupload");
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
@@ -7,10 +9,6 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const app = express();
-const { test } = require('./sql');
-const { extended, method, failed } = require('./config');
-const { generateToken } = require('./jwt');
-const { getFoldersAndFiles, makeRecursive } = require('./actioner');
 
 /* Configuration */
 app.use(bodyParser.urlencoded(extended));
@@ -22,65 +20,21 @@ app.use(cors());
 /* HTTP Methods */
 app.get('/', getFoldersAndFiles);
 
-app.get('/recursive', makeRecursive)
+app.get('/recursive', makeRecursive);
 
-app.get('/status', async (req, res) => {
-    let catchError = false;
-    const response = []
-    try {
-        const user = { name: req.body.user || 'test' }
-        const token = generateToken(user)
-        response.push(token)
-        await test().then(rows => response.push(rows));
-    } catch (error) {
-        console.log('Error', error)
-        catchError = true;
-    };
+app.get('/download', download);
 
-    if (catchError) {
-        res.status(200).json(failed);
-    } else if (!catchError) {
-        res.status(200).json({
-            success: true,
-            path: req.query.path,
-            response: response
-        });
-        res.end();
-    };
-})
+app.get('/status', status);
 
-app.get('/check', (req, res) => {
-    let catchError = false;
-    const response = []
-    try {
-        const files = fs.readdirSync(__dirname + '/data');
-        const filesWithoutExtension = [];
-        files.forEach(item => {
-            filesWithoutExtension.push(item.split('.')[0]);
-        });
-        const maxDate = new Date(Math.max.apply(null, filesWithoutExtension));
-        const lastFile = {
-            file: maxDate.getTime() + '.json'
-        }
-        res.json(lastFile)
-    } catch (error) {
-        console.log('Error', error);
-        catchError = true;
-    };
-});
+app.get('/check', check);
 
-/*
-* POST to /login
-*/
+
+/**
+ * POST to /login
+ */
 app.post('/login', (req, res) => {
     console.log('login', req.body);
 })
-
-app.get('/download', (req, res) => {
-    let fullPath = process.env.PATHTOFOLDER;
-    if (req.query.path) fullPath = fullPath + req.query.path + '/';
-    res.download(fullPath + req.query.download);
-});
 
 /*
 * POST to /
@@ -91,7 +45,7 @@ app.get('/download', (req, res) => {
 app.post('/', async (req, res) => {
     console.log(req.query, req.files)
     let fullPath = process.env.PATHTOFOLDER;
-    if (req.query.path) fullPath = fullPath + req.query.path + '/';
+    if (req.query.path) fullPath = pathChanger(fullPath, req.query.path)
     let catchError = false;
     try {
         if (req.files) {
@@ -103,8 +57,6 @@ app.post('/', async (req, res) => {
             await req.files.file.mv(fullPath + req.files.file.name)
         } else if (req.query.folder) {
             fullPath = fullPath + req.query.folder;
-            fullPath = decodeURI(fullPath);
-            fullPath = fullPath.split('%20').join(' ');
             await fs.mkdirSync(fullPath);
         } else if (req.query.edit && req.query.to) {
             await fs.renameSync(fullPath + req.query.edit, fullPath + req.query.to);
@@ -123,33 +75,7 @@ app.post('/', async (req, res) => {
     res.end();
 });
 
-app.delete('/', async (req, res) => {
-    console.log(req.query);
-    let fullPath = process.env.PATHTOFOLDER;
-    let catchError = false;
-    if (req.query.path) fullPath = fullPath + req.query.path;
-    try {
-        if (req.query.file) {
-            fullPath = fullPath + '/' + req.query.file;
-            await fs.unlinkSync(fullPath);
-        } else if (req.query.folder) {
-            fullPath = fullPath + '/' + req.query.folder;
-            if (fullPath.includes('//')) fullPath = fullPath.split('//').join('/');
-            await fs.rmSync(fullPath, { recursive: true, force: true });
-        };
-    } catch(error) {
-        console.log('Error', error);
-        catchError = true;
-    };
-    if (catchError) {
-        res.status(200).json(failed);
-    } else if (!catchError){
-        res.status(200).json({
-            success: true,
-        });
-    };
-    res.end();
-});
+app.delete('/', );
 
 app.listen(process.env.PORT, (err) => {
     if (err) console.log(err);
