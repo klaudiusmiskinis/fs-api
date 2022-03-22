f7b7fb069c479cb20c96587254e2568ced5b63c6
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-03-22 10:09:32 +0100

    Setting sections with a specifi action for a better code looking

--- sql.js
+++ sql.js
@@ -8,128 +8,15 @@ const connection = {
   database: process.env.MYSQL_DB,
 };
 
-/**
- * Selects with a count in archivos. Used for testing.
- */
-module.exports.test = test = async () => {
-  const conn = mysql.createConnection(connection);
-  try {
-    const query = util.promisify(conn.query).bind(conn);
-    return await query("SELECT count(*) as count FROM archivos");
-  } finally {
-    conn.end();
-  }
-};
-
+/* SELECTS */
 /**
  * Selects all the files from table archivos.
  */
-module.exports.selectAllFiles = selectAllFiles = async () => {
+module.exports.selectAll = selectAll = async () => {
   const conn = mysql.createConnection(connection);
   try {
     const query = util.promisify(conn.query).bind(conn);
     return await query("SELECT * FROM archivos");
-  } finally {
-    conn.end();
-  }
-};
-
-/**
- * Selects all the versions from table versiones.
- */
-module.exports.selectAllVersions = selectAllVersions = async () => {
-  const conn = mysql.createConnection(connection);
-  try {
-    const query = util.promisify(conn.query).bind(conn);
-    return await query("SELECT * FROM versiones");
-  } finally {
-    conn.end();
-  }
-};
-
-/**
- * Purges the table given by a parameter.
- */
-module.exports.purgeTable = purgeTable = async (table) => {
-  const conn = mysql.createConnection(connection);
-  try {
-    const query = util.promisify(conn.query).bind(conn);
-    const string = mysql
-      .format("TRUNCATE TABLE ?", [table])
-      .split("'")
-      .join("");
-    return await query(string);
-  } finally {
-    conn.end();
-  }
-};
-
-/**
- * Makes a massive insert into table archivos with a bulked parameter. Example: [[],[]]
- */
-module.exports.insertArchivos = insertArchivos = async (archivos) => {
-  const conn = mysql.createConnection(connection);
-  try {
-    const query = util.promisify(conn.query).bind(conn);
-    const string = mysql
-      .format(
-        "INSERT INTO archivos(nombre, ruta, fechaCreacion, ultimaVersion) VALUES ?",
-        [archivos]
-      )
-      .split("''")
-      .join("'");
-    return await query(string);
-  } finally {
-    conn.end();
-  }
-};
-
-module.exports.newFile = newFile = async (file) => {
-  const conn = mysql.createConnection(connection);
-  try {
-    const query = util.promisify(conn.query).bind(conn);
-    const string = mysql
-      .format(
-        "INSERT INTO archivos(nombre, ruta, fechaCreacion, ultimaVersion) VALUES (?)",
-        [file]
-      )
-      .split("''")
-      .join("'");
-    return await query(string);
-  } finally {
-    conn.end();
-  }
-};
-
-module.exports.newFileWithFather = newFileWithFather = async (file) => {
-  const conn = mysql.createConnection(connection);
-  try {
-    const query = util.promisify(conn.query).bind(conn);
-    const string = mysql
-      .format(
-        "INSERT INTO archivos(nombre, ruta, idPadre, fechaCreacion, ultimaVersion) VALUES (?)",
-        [file]
-      )
-      .split("''")
-      .join("'");
-    return await query(string);
-  } finally {
-    conn.end();
-  }
-};
-
-module.exports.rename = rename = async (idFile, newName) => {
-  const conn = mysql.createConnection(connection);
-  try {
-    const query = util.promisify(conn.query).bind(conn);
-    const string = mysql
-      .format("UPDATE archivos SET nombre = ? WHERE archivos.idArchivo = ?", [
-        newName,
-        idFile,
-      ])
-      .split("''")
-      .join("'");
-    return await query(string);
   } finally {
     conn.end();
   }
@@ -155,16 +42,123 @@ module.exports.selectByPathAndName = selectByPathAndName = async (
   }
 };
 
+/* TRUNCATE */
+/**
+ * Truncates the table given by a parameter.
+ */
+module.exports.truncateTable = truncateTable = async (table) => {
+  const conn = mysql.createConnection(connection);
+  try {
+    const query = util.promisify(conn.query).bind(conn);
+    const string = mysql
+      .format("TRUNCATE TABLE ?", [table])
+      .split("'")
+      .join("");
+    return await query(string);
+  } finally {
+    conn.end();
+  }
+};
 
-module.exports.ultimaVersionToZero = ultimaVersionToZero = async (idFile) => {
+/* INSERTS */
+/**
+ * Makes a massive insert into table archivos with a bulked parameter. Parameter format: [[],[]]
+ */
+module.exports.insertBulkedFiles = insertBulkedFiles = async (archivos) => {
   const conn = mysql.createConnection(connection);
   try {
     const query = util.promisify(conn.query).bind(conn);
     const string = mysql
       .format(
-        "UPDATE archivos SET ultimaVersion = 0 WHERE idArchivo = ?",
-        [idFile]
+        "INSERT INTO archivos(nombre, ruta, fechaCreacion, ultimaVersion) VALUES ?",
+        [archivos]
       )
+      .split("''")
+      .join("'");
+    return await query(string);
+  } finally {
+    conn.end();
+  }
+};
+
+module.exports.insertFile = insertFile = async (file) => {
+  const conn = mysql.createConnection(connection);
+  try {
+    const query = util.promisify(conn.query).bind(conn);
+    const string = mysql
+      .format(
+        "INSERT INTO archivos(nombre, ruta, fechaCreacion, ultimaVersion) VALUES (?)",
+        [file]
+      )
+      .split("''")
+      .join("'");
+    return await query(string);
+  } finally {
+    conn.end();
+  }
+};
+
+module.exports.insertFileWithParent = insertFileWithParent = async (file) => {
+  const conn = mysql.createConnection(connection);
+  try {
+    const query = util.promisify(conn.query).bind(conn);
+    const string = mysql
+      .format(
+        "INSERT INTO archivos(nombre, ruta, idPadre, fechaCreacion, ultimaVersion) VALUES (?)",
+        [file]
+      )
+      .split("''")
+      .join("'");
+    return await query(string);
+  } finally {
+    conn.end();
+  }
+};
+
+module.exports.insertFileWithParenAndtReason = insertFileWithParenAndtReason =
+  async (file) => {
+    const conn = mysql.createConnection(connection);
+    try {
+      const query = util.promisify(conn.query).bind(conn);
+      const string = mysql
+        .format(
+          "INSERT INTO archivos(nombre, ruta, idPadre, fechaCreacion, ultimaVersion) VALUES (?)",
+          [file]
+        )
+        .split("''")
+        .join("'");
+      return await query(string);
+    } finally {
+      conn.end();
+    }
+  };
+
+/* UPDATE */
+module.exports.updateName = updateName = async (idFile, newName) => {
+  const conn = mysql.createConnection(connection);
+  try {
+    const query = util.promisify(conn.query).bind(conn);
+    const string = mysql
+      .format("UPDATE archivos SET nombre = ? WHERE archivos.idArchivo = ?", [
+        newName,
+        idFile,
+      ])
+      .split("''")
+      .join("'");
+    return await query(string);
+  } finally {
+    conn.end();
+  }
+};
+
+module.exports.updateVersion = updateVersion = async (idFile) => {
+  const conn = mysql.createConnection(connection);
+  try {
+    const query = util.promisify(conn.query).bind(conn);
+    const string = mysql
+      .format("UPDATE archivos SET ultimaVersion = 0 WHERE idArchivo = ?", [
+        idFile,
+      ])
       .split("''")
       .join("'");
     return await query(string);
