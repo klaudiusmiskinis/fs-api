603907ff003ee5de6fbc94c3fa7fe540da0a7d84
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-04-06 09:28:53 +0200

    Cambiando archivos por files para una mejor nomenclatura de programacion

---
+++ tables/files.sql
@@ -0,0 +1,22 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `files` (
+  `id` smallint(6) NOT NULL,
+  `name` varchar(80) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `path` varchar(1024) CHARACTER SET utf8 COLLATE utf8_spanish2_ci DEFAULT '/',
+  `idParent` smallint(6) DEFAULT NULL,
+  `isLastVersion` tinyint(4) DEFAULT NULL,
+  `created_date` date DEFAULT NULL,
+  `isRemoved` tinyint(4) DEFAULT NULL,
+  `removed_date` date DEFAULT NULL,
+  `reason` varchar(300) DEFAULT 'Sin motivos.'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `files`
+  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT;
+COMMIT;
