03d92c8b683b4948b81130160a5464e6619791e0
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-04-07 10:54:54 +0200

    Removing mysql and util imports

--- config/db.js
+++ config/db.js
@@ -1,6 +1,4 @@
 require("dotenv").config();
-module.exports.util = require("util");
-module.exports.mysql = require("mysql2");
 module.exports.connection = {
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
