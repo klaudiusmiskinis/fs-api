415203a6a7ad5594d0681ca65ecb13d87acca973
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-04-07 13:05:00 +0200

    Creating constructors for objects and functions for a simple actions

---
+++ helpers/contructors.js
@@ -0,0 +1,72 @@
+const { dateToday } = require("./helpers");
+
+module.exports = {
+  deleteFile,
+  latestJsonFile,
+  setDateToName,
+  onlyLastVersion,
+  pathAndName,
+  splitDoubleSlash,
+  onlyName,
+  onlyId,
+};
+
+function onlyName(name) {
+  return {
+    name: name,
+  };
+}
+
+function onlyId(id) {
+  return {
+    id: id,
+  };
+}
+
+function pathAndName(path, name) {
+  path || "/";
+  return {
+    path: path,
+    name: name,
+  };
+}
+
+function deleteFile() {
+  return {
+    isLastVersion: 0,
+    isRemoved: 1,
+    removedDate: dateToday(),
+  };
+}
+
+function onlyLastVersion(isLastVersion) {
+  if (typeof isLastVersion !== "undefined")
+    throw "You must provide a parameter";
+  if (isLastVersion) {
+    return {
+      isLastVersion: 1,
+    };
+  } else {
+    return {
+      isLastVersion: 0,
+    };
+  }
+}
+
+function latestJsonFile(date) {
+  return {
+    file: date + ".json",
+  };
+}
+
+function setDateToName(date) {
+  return "data/" + date.getTime().toString() + ".json";
+}
+
+function splitDoubleSlash(string) {
+  return string.split("//").join("/");
+}
+
+function getNameFromRoute(route) {
+  
+}
