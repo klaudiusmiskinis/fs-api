6172a96e9b9964db377336aa0382cc67f1090a2a
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-04-07 09:29:44 +0200

    Creating console.logs to see where is enters.

--- services/file.service.js
+++ services/file.service.js
@@ -11,23 +11,28 @@ module.exports = {
 };
 
 async function getAll() {
+  console.log("Select *");
   return await db.File.findAll();
 }
 
 async function truncate() {
+  console.log("Truncate");
   await db.File.destroy({ truncate: true });
 }
 
 async function create(attributes) {
+  console.log("Create");
   if (await getFile(attributes)) {
     throw "File already exists";
   }
-  console.log(attributes)
-  const file = new db.File(attributes);
+  console.log(attributes);
+  const file = new db.File.create(attributes);
   await file.save();
 }
 
 async function bulked(files) {
+  console.log("Bulk");
+
   if (!files) throw "Files empty or without data";
   if (files.length < 1) throw "Files empty";
   await db.File.bulkCreate(files, {
@@ -36,18 +41,23 @@ async function bulked(files) {
 }
 
 async function update(attributes, conditions) {
-  const file = await db.File.update(attributes, { where: conditions });
+  console.log("Update");
+  const file = await getFile(conditions);
+  await file.update(attributes);
   if (!file) throw "File not found";
+  return file;
 }
 
 async function _delete(conditions) {
+  console.log("Delete");
   const file = await getFile(conditions);
   if (!file) throw "File not found";
   await file.destroy();
 }
 
 async function getFile(conditions) {
-  const file = db.File.findOne({ where: conditions });
+  console.log("Select");
+  const file = await db.File.findOne({ where: conditions });
   if (!file) throw "File not found";
   return file;
 }
