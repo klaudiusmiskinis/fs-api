7b4ee5c67d143b1e3b762faf5f84c41c0524778b
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-04-07 10:56:55 +0200

    Importing File instead of db.
    Optimization of functions and testing them for a correct actions.
    
    It works perfectly ✅

--- services/file.service.js
+++ services/file.service.js
@@ -1,4 +1,4 @@
-const { db } = require("../helpers/initialize");
+const { File } = require("../helpers/initialize");
 
 module.exports = {
   getAll,
@@ -7,57 +7,76 @@ module.exports = {
   update,
   bulked,
   truncate,
+  getRecursiveDown,
+  getRecursiveUp,
   delete: _delete,
 };
 
 async function getAll() {
-  console.log("Select *");
-  return await db.File.findAll();
+  return await File.findAll();
 }
 
 async function truncate() {
-  console.log("Truncate");
-  await db.File.destroy({ truncate: true });
+  await File.destroy({ truncate: true });
 }
 
 async function create(attributes) {
-  console.log("Create");
-  if (await getFile(attributes)) {
-    throw "File already exists";
-  }
-  console.log(attributes);
-  const file = new db.File.create(attributes);
+  const file = new File(attributes);
   await file.save();
 }
 
 async function bulked(files) {
-  console.log("Bulk");
-
   if (!files) throw "Files empty or without data";
   if (files.length < 1) throw "Files empty";
-  await db.File.bulkCreate(files, {
+  await File.bulkCreate(files, {
     ignoreDuplicates: true,
   }).then(() => console.log("Files data have been inserted"));
 }
 
 async function update(attributes, conditions) {
-  console.log("Update");
   const file = await getFile(conditions);
   await file.update(attributes);
   if (!file) throw "File not found";
-  return file;
 }
 
 async function _delete(conditions) {
-  console.log("Delete");
   const file = await getFile(conditions);
   if (!file) throw "File not found";
   await file.destroy();
 }
 
 async function getFile(conditions) {
-  console.log("Select");
-  const file = await db.File.findOne({ where: conditions });
+  const file = await File.findOne({ where: conditions });
   if (!file) throw "File not found";
   return file;
+}
+
+async function getRecursiveDown(conditions) {
+  let file;
+  const files = [];
+  do {
+    if (!conditions) throw "You must provide an id condition";
+    file = await getFile(conditions);
+    if (!file) throw "File not found";
+    files.push(file.dataValues);
+    conditions.id = file.dataValues.idParent;
+  } while (file.dataValues.idParent);
+  return files;
+}
+
+async function getRecursiveUp(conditions) {
+  let file;
+  const files = [];
+  do {
+    if (!conditions) throw "You must provide an idParent condition";
+    try {
+      file = await getFile(conditions);
+      if (!file) throw "File not found";
+      files.push(file.dataValues);
+      conditions.idParent = file.dataValues.id;
+    } catch {
+      file.dataValues.id = null;
+    }
+  } while (file.dataValues.id);
+  return files;
 }
